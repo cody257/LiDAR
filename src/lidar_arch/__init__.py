e37768a4b30e3450bddfa15eb570e6b8a60962a9
Cody <cody@copperstateit.com>
2026-06-07 09:18:40 -0700

    chore: scaffold lidar-arch package

---
+++ src/lidar_arch/__init__.py
@@ -0,0 +1,2 @@
+"""lidar-arch: archaeology-optimized terrain visualizations from USGS 3DEP LiDAR."""
+__version__ = "0.1.0"
---
+++ tests/test_smoke.py
@@ -0,0 +1,5 @@
+import lidar_arch
+
+
+def test_package_imports():
+    assert lidar_arch.__version__ == "0.1.0"
